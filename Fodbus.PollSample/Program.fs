08c2855d10ca9f3d8287187fae33ae866242183f
newbienewbie <itminus@hotmail.com>
2022-09-29 18:17:07 +0800

    预定义针脚配置

--- Fodbus.PollSample/Program.fs
+++ Fodbus.PollSample/Program.fs
@@ -6,27 +6,72 @@ open System.Threading.Tasks
 open Itminus.Fodbus
 open Itminus.Fodbus.Indication
 open Itminus.Fiddlewares.Middleware
-open FsToolkit.ErrorHandling
 
 
 let handleBtnPressedMiddleware hintPath btnPin perform ctrl = 
     let h = handleBtnPressed hintPath btnPin ctrl perform 
     toContinuation ctrl h
 
-let HintPin_放行_提示灯 = DOPinAddr.DO5
-let Btn_放行_执行键 = DIPinAddr.DI2
+
+[<CLIMutableAttribute>]
+type ZLanPinsConfiguration = {
+
+    AndonPinRed    : DOPinAddr
+    AndonPinYellow : DOPinAddr
+    AndonPinRight  : DOPinAddr
+    AndonPinSound  : DOPinAddr
+
+
+    ResetPin_提示灯 : DOPinAddr
+    ResetBtn_执行键 : DIPinAddr
+    
+
+    HintPin_放行_提示灯 : DOPinAddr
+    Btn_放行_执行键 : DIPinAddr
+}
+
+
+type Alarm = 
+    | Critical
+    | Warning
+
+type Model = {
+    Alarms: Alarm option
+}
+
+let PINS_CONFIG: ZLanPinsConfiguration = {
+    AndonPinRed    = DOPinAddr.DO1
+    AndonPinYellow = DOPinAddr.DO2
+    AndonPinRight  = DOPinAddr.DO3
+    AndonPinSound  = DOPinAddr.DO4
+
+    ResetPin_提示灯 = DOPinAddr.DO5
+    ResetBtn_执行键 = DIPinAddr.DI2
+
+    HintPin_放行_提示灯 = DOPinAddr.DO6
+    Btn_放行_执行键 = DIPinAddr.DI6
+}
+
 
 let MW_放行_处理  = fun ctrl ->
     let perform: Perform<unit, string> = fun ctrl ->
         printfn "已执行"
         task { return Ok ()} 
-    handleBtnPressedMiddleware HintPin_放行_提示灯 Btn_放行_执行键  perform ctrl
+    handleBtnPressedMiddleware 
+        PINS_CONFIG.HintPin_放行_提示灯 
+        PINS_CONFIG.Btn_放行_执行键  
+        perform 
+        ctrl
 
 let MW_放行_提示 = fun (ctrl: ZLanCtrl) ->
     let preflight: Preflight<string> = fun ctrl -> 
         printfn "正在飞检"
         task { return Ok () }
-    setHintOnWhen HintPin_放行_提示灯 Btn_放行_执行键 ctrl preflight
+    setHintOnWhen 
+        PINS_CONFIG.HintPin_放行_提示灯 
+        PINS_CONFIG.Btn_放行_执行键 
+        ctrl 
+        preflight
     |> toContinuation ctrl 
 
 let mw_set_toggle pin ctrl = 
